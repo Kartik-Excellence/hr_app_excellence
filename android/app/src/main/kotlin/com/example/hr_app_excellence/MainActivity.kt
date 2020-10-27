1629795b69b32c7122d634016bbb95f4e127e84a
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-10-27 13:06:40 +0530

    first commit to main

---
+++ android/app/src/main/kotlin/com/example/hr_app_excellence/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hr_app_excellence
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return LoginScreen();
+  }
+}
